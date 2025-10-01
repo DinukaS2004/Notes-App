fcf514081959696dba452a4ea1cbc4438c03e3e8
DinukS2004 <dinukad735@gmail.com>
2025-10-01 20:27:17 +0530

    fixed small issues

--- frontend/notes-app/src/Pages/Login/Login.jsx
+++ frontend/notes-app/src/Pages/Login/Login.jsx
@@ -35,14 +35,17 @@ const Login = ()=> {
         <div className='w-96 border rounded bg-white px-7 py-10'>
           <form onSubmit={handleLogin}>
             <h4 className='text-2xl mb-7'>Login </h4>
-            
-            <input type='text'placeholder='Email'className='input-box'
-            
-            value={email}
-              onChange={(e)=> setEmail(e.target.value)}
-              />
 
-            <Passwordinput value={password}
+            <input 
+            type='text'
+            placeholder='Email'
+            className='input-box'
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+            />
+
+            <Passwordinput 
+              value={password}
               onChange={(e)=> setPassword(e.target.value)}
             />
 
--- frontend/notes-app/src/Pages/Signup/Signup.jsx
+++ frontend/notes-app/src/Pages/Signup/Signup.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
+import Passwordinput from '../../Components/Input/Passwordinput'
+import { validateEmail } from '../../../utils/helper'
 
-const 
+const Signup = () =>{
 
-function Signup() {
-  return (
+  const [name, setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error,setError] = useState(null);
+
+  const handleSignUp = async (e) =>{
+
+    e.preventDefault();
+
+    if(!name){
+      setError("Please enter your name");
+      return;
+    }
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password");
+      return;
+    }
+    
+    setError('');
+  };
+
+  return(
     <>
       <Navbar/>
 
@@ -11,11 +41,43 @@ function Signup() {
         <div className='w-96 border rounded bg-white px-7 py-10'>
           <form onSubmit={handleSignUp}>
             <h4 className='text-2xl mb-7'>Login </h4>
+
+            <input 
+            type='text'
+            placeholder='Name'
+            className='input-box'
+            value={name}
+            onChange={(e)=> setName(e.target.value)}
+            />
+
+            <input 
+            type='text'
+            placeholder='Email'
+            className='input-box'
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+            />
+
+            <Passwordinput 
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+            />
+
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
+            <button type='submit'className='btn-primary'>Create Account</button>
+            <p className='text-sm text-center mt-4'>
+              Already have an account? {" "}
+              <Link to="/Signup" className="font-medium text-primary underline">
+              Login
+              </Link>
+            </p>
+
            </form>
            </div>
            </div>
-      </> 
-  )
-}
+    </> 
+  );
+};
 
-export default Signup
+export default Signup;
