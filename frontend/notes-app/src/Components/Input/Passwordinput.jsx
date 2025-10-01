449c80f40508f47d060df6f54a8362f612a3bb1a
DinukS2004 <dinukad735@gmail.com>
2025-10-01 19:45:20 +0530

    Password Input component

--- frontend/notes-app/src/Components/Input/Passwordinput.jsx
+++ frontend/notes-app/src/Components/Input/Passwordinput.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {FaRegEye,FaRegEyeSlash} from "react-icons/fa6";
 
 const PasswordInput = ({value,onChange,placeholder})=>{
 
@@ -15,7 +16,21 @@ const PasswordInput = ({value,onChange,placeholder})=>{
         onChange = {onChange}
         type = {isShowPassword ? "text":"password"}
         placeholder={placeholder || "Password"}
-        className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none '/>
+        className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none '
+        />
+
+        {isShowPassword ?(
+            <FaRegEye
+            size={22}
+            className = "text-primary cursor-pointer"
+            onclick = {()=> toggleShowPassword()}
+            />
+        ): ( <FaRegEyeSlash
+            size = {22}
+            className='text-slate-400 cursor-pointer'
+            onClick={()=> toggleShowPassword()}
+            />
+        )}
     </div>
   );
 };
