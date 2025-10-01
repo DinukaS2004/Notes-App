01e3fac317cdeec27ac3dee7a2a86383fbda990d
DinukS2004 <dinukad735@gmail.com>
2025-10-01 14:29:12 +0530

    Password Input

--- frontend/notes-app/src/Pages/Login/Login.jsx
+++ frontend/notes-app/src/Pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
+import Passwordinput from '../../Components/Input/Passwordinput'
 
 function Login() {
   return (
@@ -12,6 +13,9 @@ function Login() {
           <form onSubmit={()=> {}}>
             <h4 className='text-2xl mb-7'>Login </h4>
             <input type='text'placeholder='Email'className='input-box'/>
+
+            <Passwordinput />
+            
             <button type='submit'className='btn-primary'>Login</button>
             <p className='text-sm text-center mt-4'>
               Not registered yet? {" "}
